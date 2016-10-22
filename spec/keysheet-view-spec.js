b9f19c189e46147870274bdc95305fb0b89aa387
Pedro Casaubon <xperiments@gmail.com>
2016-10-23 00:20:13 +0200

    First commit

---
+++ spec/keysheet-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KeysheetView from '../lib/keysheet-view';
+
+describe('KeysheetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
